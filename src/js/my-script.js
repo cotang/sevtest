9a63888cd5376b4dd48bbb7f469853499a9c630d
Konstantin Bogdanov <kbogdanov1@gmail.com>
2016-07-21 13:23:58 +0300

    adaptive

--- src/js/my-script.js
+++ src/js/my-script.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 
-    var up = $('.page-footer__up');
+    $('.services__label').click(function() {
+        $(this).children('.fa').toggleClass('fa-chevron-up fa-chevron-down');
+        $(this).siblings('.services__content').slideToggle('slow');      
+   });
+
+
     $('.page-footer__up').click(function(e) {
         e.preventDefault();
         $('html, body').animate({scrollTop: 0},500);
@@ -8,18 +13,33 @@ $(document).ready(function(){
     });
 
 
-
+    var lastScreen = $(document).height() - $(window).height();
+    var buttonsPosition = lastScreen - (20 + 42);
     var buttons = $('.footer-buttons');
     function scroll() {
-        if ($(window).scrollTop() > x) {
-            buttons.css("bottom", "0");
-        } else if ($(window).scrollTop() > x) {
-            buttons.css("bottom", "20px");
+        if ($(window).scrollTop() >= buttonsPosition) {
+              if ($(window).width() < 769) {
+                buttons.animate({"bottom": "310px"}, 100);
+              } else {
+                buttons.animate({"bottom": "175px"}, 100);
+              };
+        } else if ($(window).scrollTop() >= 50) {
+            buttons.animate({"bottom": "20px"}, 100);
         } else {
-            buttons.css("bottom", "125px");
+            buttons.css("bottom", "auto");
         }
     }
     document.onscroll = scroll;
 
 
+    //console.log(lastScreen);
+    //console.log(buttonsPosition);
+
+  // if ($(window).width() < 769) {
+  //   buttons.animate({"bottom": "310px"}, 100);
+  // } else {
+  //   buttons.animate({"bottom": "175px"}, 100);
+  // }
+
 });
+
